93b1037747e6896fb3418556c68aa9c3c69c2b1b
Harsha <gg9488@srmist.edu.in>
2026-04-22 22:50:45 +0530

    [Harsha] added [UC17]

--- App/src/Train_Consist_Management_App.java
+++ App/src/Train_Consist_Management_App.java
@@ -1,30 +1,25 @@
+import java.util.Arrays;
+
 public class Train_Consist_Management_App {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
 
         System.out.println("\nBefore Sorting:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println(Arrays.toString(bogieNames));
 
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - i - 1; j++) {
+        Arrays.sort(bogieNames);
 
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-
-        System.out.println("\n\nAfter Sorting (Ascending Order):");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("\nAfter Sorting (Alphabetical Order):");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
